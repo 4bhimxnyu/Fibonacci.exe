ed96630b900e7f860d3dae346443bb9f8a86bfa2
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-12-11 20:40:11 +0530

    cursor added

---
+++ music.js
@@ -0,0 +1,10 @@
+const playAbnormal=() =>{
+    audio1.play();
+}
+const audio1 = new Audio(".\Audio\Daft Punk (feat. Julian Casablancas) - Instant Crush [Random Access Memories].mp3");
+
+// Play the audio
+
+
+// Pause the audio
+audio.pause();
