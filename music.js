53284b3323b04d6047c67df16fc214d19f19d4df
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-12-15 01:14:47 +0530

    audio bar /all

--- music.js
+++ music.js
@@ -4,6 +4,25 @@ let play3 = document.getElementById("play3");
 let play4 = document.getElementById("play4");
 
 function playStrokes(){
-    play1.style.display = "block";
-    
+    // play1.style.display = "block";
+    let audio = new Audio("./Audio/The Strokes - Why Are Sundays So Depressing (Audio).mp3")
+    audio.play();
+   
 }
+
+play1.addEventListener("click" , playStrokes)
+
+play2.addEventListener("click" , ()=>{
+    let audio = new Audio("./Audio/Radiohead - Jigsaw Falling into Place [HD].mp3");
+    audio.play();
+})
+
+play3.addEventListener("click" , ()=>{
+    let audio = new Audio("./Audio/Daft Punk (feat. Julian Casablancas) - Instant Crush [Random Access Memories].mp3");
+    audio.play();
+})
+
+play4.addEventListener("click" , ()=>{
+    let audio = new Audio("./Audio/Travis Scott - SICKO MODE (Lyrics) ft. Drake.mp3");
+    audio.play();
+})
