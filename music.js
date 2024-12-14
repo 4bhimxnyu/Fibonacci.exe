95f8ce6a4979802f59ed7619697da17395cf59f6
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-12-15 00:41:17 +0530

    audio bar 1

--- music.js
+++ music.js
@@ -1,10 +1,9 @@
-const playAbnormal=() =>{
-    audio1.play();
+let play1 = document.getElementById("play1");
+let play2 = document.getElementById("play2");
+let play3 = document.getElementById("play3");
+let play4 = document.getElementById("play4");
+
+function playStrokes(){
+    play1.style.display = "block";
+    
 }
-const audio1 = new Audio(".\Audio\Daft Punk (feat. Julian Casablancas) - Instant Crush [Random Access Memories].mp3");
-
-// Play the audio
-
-
-// Pause the audio
-audio.pause();
