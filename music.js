8ade6330ac06b814244eafc062feb6fa61f9712d
abhimanyu <abhimanyupratapsingh30nov1106@gmail.com>
2024-12-16 11:00:37 +0530

    added timeout function

--- music.js
+++ music.js
@@ -7,6 +7,10 @@ function playStrokes(){
     // play1.style.display = "block";
     let audio = new Audio("./Audio/The Strokes - Why Are Sundays So Depressing (Audio).mp3")
     audio.play();
+    setTimeout(() => {
+        audio.pause();
+        audio.currentTime = 0; // Reset to the beginning
+    }, 8000);
    
 }
 
@@ -15,32 +19,27 @@ play1.addEventListener("click" , playStrokes)
 play2.addEventListener("click" , ()=>{
     let audio = new Audio("./Audio/Radiohead - Jigsaw Falling into Place [HD].mp3");
     audio.play();
+    setTimeout(() => {
+        audio.pause();
+        audio.currentTime = 0; // Reset to the beginning
+    }, 8000);
 })
 
 play3.addEventListener("click" , ()=>{
     let audio = new Audio("./Audio/Daft Punk (feat. Julian Casablancas) - Instant Crush [Random Access Memories].mp3");
     audio.play();
+    setTimeout(() => {
+        audio.pause();
+        audio.currentTime = 0; // Reset to the beginning
+    }, 8000);
 })
 
 play4.addEventListener("click" , ()=>{
     let audio = new Audio("./Audio/Travis Scott - SICKO MODE (Lyrics) ft. Drake.mp3");
     audio.play();
+    setTimeout(() => {
+        audio.pause();
+        audio.currentTime = 0; // Reset to the beginning
+    }, 8000);
 })
 
-let currentAudio = null;
-
-document.querySelectorAll('.button').forEach(button => {
-  button.addEventListener('click', () => {
-    const audioSrc = song.getAttribute('audio');
-
-    // Stop the currently playing audio if it exists
-    if (currentAudio) {
-      currentAudio.pause();
-      currentAudio.currentTime = 0; // Reset the audio to the beginning
-    }
-
-    // Create a new audio element and play the clicked song
-    currentAudio = new Audio(audioSrc);
-    currentAudio.play();
-  });
-});
